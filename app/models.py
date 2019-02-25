edcc641c0e928c74ca4581609cbacb4d6b8c1073
luiguip <luisguilhermepdea@gmail.com>
2019-02-25 19:15:33 -0300

    Chapter 15

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
+from flask import current_app
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 from datetime import datetime
 from time import time
 import jwt
-from app import app, db, login
+from app import db, login
 
 
 followers = db.Table(
@@ -66,13 +67,13 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
             id = jwt.decode(
-                token, app.config['SECRET_KEY'],
+                token, current_app.config['SECRET_KEY'],
                 algorithms=['HS256'])['reset_password']
         except:
             return
